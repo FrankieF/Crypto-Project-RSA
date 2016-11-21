5731e1730041bd6c23b2c7840f6b82b974f567f4
Bufft25 <bufft25@students.rowan.edu>
2016-11-21 12:22:34 -0500

    made modPower and Inverse.
    Not final needs more testing

--- RSA.java
+++ RSA.java
@@ -8,7 +8,8 @@ package project_3;
 public class RSA {
 	
 	public static void main(String[] args){
-		System.out.print(modPower(43,593239810,73));
+		System.out.println(modPower(75,1,17));
+		System.out.println(inverse(17,75));
 	}
 	
 	public RSA(){}
@@ -20,7 +21,39 @@ public class RSA {
 	 * @return inverse of e mod m
 	 */
 	public static long inverse(long e, long m){
-		return 0;
+		//r1/r2 = q and next r2
+		long r1 = m,r2 = e, swap = 0;
+		long u = 0, uMin2 = 0, uMin1 = 1;
+		long v = 0, vMin2 = 1, vMin1 = 0;
+		long q = 0;
+		while(r2 != 1){
+			//gets the quotient
+			q = r1/r2;
+			//swap holds value r2 for use later.
+			swap = r2;		
+			//gets the next r2 value
+			r2 = modPower(r1,1,r2);
+			r1 = swap;
+			
+			u = uMin2 - q * uMin1;
+			uMin2 = uMin1;
+			uMin1 = u;
+			v = vMin2 - q * vMin1;
+			vMin2 = vMin1;
+			vMin1 = v;	
+			
+			//for error checking to make sure euclidean alg is working correctly.
+			if(r2 != (u*e + v*m)){
+				System.err.println("The value for: "+ r2 + " has the wrong u or v");
+			}
+			
+		}
+		if(u < 0){
+			u = u + m;
+			return u;
+		}else{
+		return u;
+		}
 	}
 	
 	/**
